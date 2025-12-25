787fa8421fcac9e5ade4d0f28b704b4322438bd9
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-25 23:35:44 +0000

    model: Client.java attributes

--- src/main/java/com/pankassi/domain/model/Client.java
+++ src/main/java/com/pankassi/domain/model/Client.java
@@ -3,6 +3,8 @@ package com.pankassi.domain.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Builder
 @NoArgsConstructor
@@ -15,4 +17,30 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "clientId")
     private Long clientId;
+
+    @Column(name = "firstName",nullable = false,length = 50)
+    private String firstName;
+
+    @Column(name = "lastName",nullable = false,length = 50)
+    private String lastName;
+
+    @Column(name = "userName",nullable = false,length = 50)
+    private String userName;
+
+    @Column(name = "email",nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "password",nullable = false,length = 50)
+    private String password;
+
+    @Column(name = "isActive",nullable = false,length = 1)
+    private boolean isActive;
+
+    @Column(name = "createdAt",nullable = false)
+    private LocalDateTime createdAt;
+
+    //Business Method
+    public String getFullName(){
+        return firstName+" "+lastName;
+    }
 }
