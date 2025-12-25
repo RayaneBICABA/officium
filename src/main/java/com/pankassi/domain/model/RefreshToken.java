92f84341f10b08e55d39e907bad104637f60235a
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-25 22:54:59 +0000

    model: RefreshToken init

--- src/main/java/com/pankassi/domain/model/RefreshToken.java
+++ src/main/java/com/pankassi/domain/model/RefreshToken.java
@@ -1,7 +1,6 @@
 package com.pankassi.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -10,6 +9,10 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "REFRESHTOKEN")
 public class RefreshToken {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "refreshTokenId")
+    private Long refreshTokenId;
 }
