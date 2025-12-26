ca92e9a03356e57f08811ffa2c1a31e5c17babdf
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-26 00:05:39 +0000

    model: RefreshToken.java attributes added

--- src/main/java/com/pankassi/domain/model/RefreshToken.java
+++ src/main/java/com/pankassi/domain/model/RefreshToken.java
@@ -3,16 +3,28 @@ package com.pankassi.domain.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "REFRESHTOKEN")
+@Table(name = "REFRESH_TOKEN")
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "refreshTokenId")
     private Long refreshTokenId;
+
+    @Column(name = "createdAt",nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expiresAt",nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "token",nullable = false)
+    private String token;
+    
 }
