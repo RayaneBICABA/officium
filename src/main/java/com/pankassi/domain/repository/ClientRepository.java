024152c72aab447fca3c1b5133d2b93e2f25ee4c
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-27 19:22:56 +0000

    repository: ClientRepository.java query added for registration and login features

--- src/main/java/com/pankassi/domain/repository/ClientRepository.java
+++ src/main/java/com/pankassi/domain/repository/ClientRepository.java
@@ -2,7 +2,27 @@ package com.pankassi.domain.repository;
 
 import com.pankassi.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    /**
+     * Already existing methods:
+     * - save()
+     * - findById()
+     * - findAll()
+     * - deleteById()
+     * - count()
+     * - existsBy()
+     */
+
+    //Check if email
+    boolean findByEmail(String email);
+
+    // Collect password using email
+    @Query("SELECT c.password FROM Client WHERE c.email = :email")
+    Optional<String> findPasswordByEmail(@Param("email") String email);
 
 }
---
+++ src/main/java/com/pankassi/dto/authDto/RegistrationDto.java
@@ -0,0 +1,4 @@
+package com.pankassi.dto.authDto;
+
+public record RegistrationDto() {
+}
