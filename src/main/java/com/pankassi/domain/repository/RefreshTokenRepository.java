f63e2b9650a11a175036dd4e128c4c047cd08888
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-27 17:16:04 +0000

    repository: RefreshTokenRepository.java init

--- src/main/java/com/pankassi/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/pankassi/domain/repository/RefreshTokenRepository.java
@@ -1,4 +1,8 @@
 package com.pankassi.domain.repository;
 
-public interface RefreshTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pankassi.domain.model.RefreshToken;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
+
 }
