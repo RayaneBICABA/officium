95d8aaaf72834a0f8ef05e27edbb473f039e608a
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-12-29 20:14:44 +0000

    service: AuthService init

---
+++ src/main/java/com/pankassi/service/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.pankassi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // Permet de generer automatiquement un constructeur
+public class AuthenticationService {
+
+}
